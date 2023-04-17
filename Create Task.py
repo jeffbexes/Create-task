581245239004d9a2b91293e316114693b0e89f97
jeffbexes <96642253+jeffbexes@users.noreply.github.com>
2023-04-16 20:53:10 -0700

    Update Create Task.py

--- Create Task.py
+++ Create Task.py
@@ -3,13 +3,19 @@ from os import path
 import tkinter as Tk
 import random as rand
 import time as t
-#images
 
-wn = trtl.Screen()
+#shit
+key_pen=trtl.Turtle
 
+#setup
 score=0
-
-
+upgrades=0
+points=1
+fertilizer_cost=2000
+plantation_cost = 200
+silver_cost = 20
+wn = trtl.Screen()
+#shapes
 wn.register_shape("banana.gif")
 clicker = trtl.Turtle()
 clicker.shape("banana.gif")
@@ -47,8 +53,6 @@ fert.goto(150,200)
 
 
 #setup
-wn = trtl.Screen()
-
 font_setup = ("Arial", 40, "normal")
 score_pen = trtl.Turtle()
 score_pen.penup()
@@ -56,14 +60,15 @@ score_pen.hideturtle()
 score_pen.goto(0,300)
 
 points = 1
-upgrades = ["smol bananana plantation.gif","Factory-1.gif"]
-banana.pop(upgrades,0)
+upgrade = ["smol bananana plantation.gif","Factory-1.gif"]
+purchases=upgrade.pop(upgrades)
 
-score = 0
+
 #Variables
-
+#Upgrades
 def twox_upgrade():
   global points
+  global score
   global silver_cost
   if score <= silver_cost:
     print("nothing")
@@ -72,9 +77,38 @@ def twox_upgrade():
     score = score- silver_cost
     silver_cost = silver_cost + 200
     update_score()
-
-
-
+    
+def fourx_upgrade():
+    global points
+    global score
+    global plantation_cost
+    if score <= plantation_cost:
+        print("nothing for four")
+    else:
+        points=points*4
+        score=score-plantation_cost
+        plantation_cost=plantation_cost+600
+        
+def eightx_upgrade():
+    global points
+    global score
+    global fertilizer_cost
+    if  score<=fertilizer_cost:
+        print("nothing for eight")
+    else:
+        points=points*8
+        score=score-fertilizer_cost
+        fertilizer_cost=fertilizer_cost*2
+        
+        
+'''def key():
+  key_pen.penup()
+  key_pen.goto(300,300)
+  key_pen.pendown()
+  key_pen.write("space is +",points,"bananas")
+  key_pen.penup()
+  key_pen.goto(100, 150)
+  key_pen.write("G is silver bananan upgrade")'''
 px = 100
 py = 0
 def plantation():
@@ -97,11 +131,13 @@ def update_score():
     (score_pen.write(score,font=font_setup))
 
 
-plantation_cost = 200
-silver_cost = 20
+
 
 wn.onkeypress(update_score, "space")
 wn.onkeypress(twox_upgrade, "g")
+wn.onkeypress(fourx_upgrade, "h")
+wn.onkeypress(eightx_upgrade, "j")
+
 
 wn.listen()
 wn.mainloop()
